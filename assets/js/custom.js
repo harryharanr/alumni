8d1fc7c2452f491a44817352c34416f32eb382ab
Hariharan Ramanan <harryharanr@gmail.com>
2018-07-15 01:41:27 +0530

    Initial Setup done

---
+++ assets/js/custom.js
@@ -0,0 +1,12 @@
+"use strict";
+
+jQuery(document).ready(function($){
+    //your custom js code here
+    $('#datatable').DataTable({
+        "info":false,
+        "paging":true,
+        "ordering": false
+    });
+});
+
+
